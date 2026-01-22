18788afe481a895bb2347d87640825a611070fda
Ajay Kumar <ajaykrsingh8709@gmail.com>
2026-01-23 02:04:23 +0530

    style selector done

--- frontend/src/pages/Generate.tsx
+++ frontend/src/pages/Generate.tsx
@@ -2,8 +2,10 @@ import { useState } from "react";
 import { useParams } from "react-router-dom"
 // import { IThumbnail } from "../assets/assets";
 import SoftBackdrop from "../components/SoftBackdrop";
-import type { IThumbnail } from "../assets/assets";
+import { colorSchemes, type AspectRatio, type IThumbnail, type ThumbnailStyle } from "../assets/assets";
 import { button } from "motion/react-client";
+import AspectRatioSelector from "../components/AspectRatioSelector";
+import StyleSelector from "../components/StyleSelector";
 
 
 const Generate = () => {
@@ -12,6 +14,11 @@ const Generate = () => {
     const [additionalDetails,setAdditionalDetails] = useState(" ")
     const [thumbnail,setThumbnail] = useState<IThumbnail | null>(null)
     const [loading,setLoading]= useState(false)
+    const [aspectRatio,setAspectRatio] = useState<AspectRatio>('16:9')
+    const [colorSchemeId,setColorSchemeId] = useState<string>(colorSchemes[0].id)
+    const [style,setStyle] = useState<ThumbnailStyle>('Bold & Graphic')
+    const [styleDropdownOpen,setStyleDropdownOpen] = useState(false)
+
 
   return (
     <>
@@ -36,7 +43,9 @@ const Generate = () => {
                                 </div>
                             </div>
                             {/* AspectRatioSeclector */}
+                            <AspectRatioSelector value = {aspectRatio }onChange = {setAspectRatio} />
                             {/* StyleSelector */}
+                            <StyleSelector value={style} onChange={setStyle} isOpen={styleDropdownOpen} setIsOpen ={setStyleDropdownOpen} />
                             {/* ColorSchemeSelector */}
 
                             {/* DETAILS */}
